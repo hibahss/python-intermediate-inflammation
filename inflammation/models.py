41f2f599daf705ff3c2f47a3c69f9cf65c674f4e
hibahss <204799517+hibahss@users.noreply.github.com>
2025-04-29 11:12:02 +0100

    standardise std functions
    Co-authored-by: SimoneAusilio <85282278+SimoneAusilio@users.noreply.github.com>

--- inflammation/models.py
+++ inflammation/models.py
@@ -33,12 +33,6 @@ def daily_min(data):
     return np.min(data, axis=0)
 
 
-def s_dev(data):
+def daily_std(data):
     """Computes and returns standard deviation for data."""
-    mmm = np.mean(data, axis=0)
-    devs = []
-    for entry in data:
-        devs.append((entry - mmm) * (entry - mmm))
-
-    s_dev2 = sum(devs) / len(data)
-    return {'standard deviation': s_dev2}
+    return np.std(data, axis=0)
